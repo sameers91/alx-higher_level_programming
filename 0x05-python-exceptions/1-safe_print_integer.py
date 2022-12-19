79a90b304ff2fdf777df3e0d353efdf5e7a9bbfa
Sameer Moosa <sammoosa.sm@gmail.com>
2022-12-19 10:08:21 -0800

    code

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+
+
+
+def safe_print_integer(value):
+    
+    try:
+        
+        print("{:d}".format(value))
+        
+        return (True)
+    
+    except (TypeError, ValueError):
+        
+        return (False)
